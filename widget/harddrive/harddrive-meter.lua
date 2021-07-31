7c5533caa91d9e6970c51920e95f7e45cc05e187
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-07-30 23:32:13 -0700

    left-panel annoyance fixed

--- widget/harddrive/harddrive-meter.lua
+++ widget/harddrive/harddrive-meter.lua
@@ -1,47 +1,63 @@
---  _______                __     __        __                   _______         __
--- |   |   |.---.-.----.--|  |.--|  |.----.|__|.--.--.-----.    |   |   |.-----.|  |_.-----.----.
--- |       ||  _  |   _|  _  ||  _  ||   _||  ||  |  |  -__|    |       ||  -__||   _|  -__|   _|
--- |___|___||___._|__| |_____||_____||__|  |__| \___/|_____|    |__|_|__||_____||____|_____|__|
--- ########################################################################
--- ########################################################################
--- ########################################################################
 local wibox = require("wibox")
-local mat_list_item = require("module.interface.material.list-item")
 local mat_slider = require("module.interface.progress_bar")
-local mat_icon = require("module.interface.material.icon")
 local icons = require("theme.icons")
 local dpi = require("beautiful").xresources.apply_dpi
+local config = require("module.functions")
+local file = require("module.functions.file")
 local signals = require("module.settings.signals")
--- ########################################################################
--- ########################################################################
--- ########################################################################
+local delayed_timer = require("lib.function.delayed-timer")
+local beautiful = require("beautiful")
+local noNetwork = true
+local gears = require("gears")
+local vicious = require("lib.vicious")
+
 local slider =
     wibox.widget {
     read_only = true,
-    widget = mat_slider
+    widget = mat_slider,
+    forced_height = 32,
+    forced_width = 32
 }
--- ########################################################################
--- ########################################################################
--- ########################################################################
 signals.connect_disk_usage(
     function(usage)
         slider:set_value(usage)
     end
 )
--- ########################################################################
--- ########################################################################
--- ########################################################################
+
+local widget_icon =
+    wibox.widget {
+    id = "icon",
+    image = icons.disc,
+    forced_width = 36,
+    forced_height = 36,
+    widget = wibox.widget.imagebox
+}
+
+local fs = wibox.widget.textbox()
+vicious.cache(vicious.widgets.fs)
+vicious.register(fs, vicious.widgets.fs, "${/ used_p}%")
+fs.font = beautiful.font .. " 14"
+
 local harddrive_meter =
     wibox.widget {
-    wibox.widget {
-        icon = icons.harddisk,
-        size = dpi(24),
-        widget = mat_icon
+    {
+        {
+            widget_icon,
+            fs,
+            spacing = dpi(1),
+            layout = wibox.layout.flex.horizontal
+        },
+        top = dpi(2),
+        bottom = dpi(2),
+        left = dpi(1),
+        right = dpi(1),
+        widget = wibox.container.margin
     },
-    slider,
-    widget = mat_list_item
+    shape = gears.shape.rounded_rect,
+    bg = beautiful.bg_normal .. "77",
+    shape_border_color = beautiful.bg_normal,
+    shape_border_width = dpi(3),
+    widget = wibox.container.background
 }
--- ########################################################################
--- ########################################################################
--- ########################################################################
+
 return harddrive_meter
