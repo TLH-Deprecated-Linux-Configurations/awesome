3c2e972251854e3aeb69f88a92ac1d4459ba7af6
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-08-24 17:31:33 -0700

    stable no double taglist

--- rc.lua
+++ rc.lua
@@ -18,16 +18,7 @@ require 'awful.autofocus'
 require('awful.hotkeys_popup.keys')
 -- ########################################################################
 require('configuration')
--- Screen Padding and Tags
-screen.connect_signal(
-    'request::desktop_decoration',
-    function(s)
-        -- Screen padding
-        screen[s].padding = {left = 0, right = 0, top = 0, bottom = 0}
-        -- Each screen has its own tag table.
-        awful.tag({'1', '2', '3', '4', '5'}, s, awful.layout.layouts[1])
-    end
-)
+
 -- ########################################################################
 -- ########################################################################
 -- ########################################################################
