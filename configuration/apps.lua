4c6b32a60a554876962c40e8de7514dcdf815ba8
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-12-28 06:43:44 -0800

    --- Auto-Git Commit ---

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -1,75 +1,96 @@
---  _______                    
+--  _______
 -- |   _   |.-----.-----.-----.
 -- |       ||  _  |  _  |__ --|
 -- |___|___||   __|   __|_____|
---          |__|  |__|         
+--          |__|  |__|
 -- ########################################################################
 -- ########################################################################
 -- ########################################################################
 return {
+    -- ########################################################################
     -- The default applications that we will use in keybindings and widgets
     --
     default = {
         -- Default terminal emulator
         --
         terminal = 'kitty',
+        -- ########################################################################
         -- Default web browser
         --
         browser = 'firefox-nightly',
+        -- ########################################################################
         -- Default text editor
         text_editor = 'nvim',
+        -- ########################################################################
         -- Default file manager
         file_manager = 'caja',
+        -- ########################################################################
         -- Default media player
+        --
         multimedia = 'vlc',
+        -- ########################################################################
         -- Default graphics editor
+        --
         graphics = 'gimp',
+        -- ########################################################################
         -- Default network manager
+--
         network_manager = 'sh ' .. HOME .. '/.config/awesome/bin/wifi.sh',
+        -- ########################################################################
         -- Default bluetooth manager
         bluetooth_manager = 'blueman',
+        -- ########################################################################
         -- Default power manager
+        --
         power_manager = 'xfce4-power-manager',
+        -- ########################################################################
         -- Default GUI package manager
+        --
         package_manager = 'pamac-manager',
+        -- ########################################################################
         -- Default locker
+        --
         lock = 'bash ' .. HOME .. '/.config/awesome/blur.sh',
+        -- ########################################################################
         -- Default quake terminal
+        --
         quake = 'kitty --name QuakeTerminal',
+        -- ########################################################################
         -- Default app menu
-        rofi_appmenu = ' bash ' ..
-            HOME ..
-                '/.config/awesome/bin/applauncher.sh ' ..
-                    screen.primary.dpi .. ' ' .. filesystem.get_configuration_dir()
-
-        -- You can add more default applications here
+        --
+        rofi_appmenu = ' bash ' .. HOME ..
+            '/.config/awesome/bin/applauncher.sh ' .. screen.primary.dpi .. ' ' ..
+            filesystem.get_configuration_dir()
+        -- ########################################################################
+        
+        --
     },
+    -- ########################################################################
     -- List of apps to start once on start-up
     run_on_start_up = {
-        'xsettingsd &',
-        'xrdb "$HOME"/.Xresources &',
-        'pnmixer &',
+        'xsettingsd &', 'xrdb "$HOME"/.Xresources &', 'pnmixer &',
         'xinpt set-prop "ELAN1301:00 04F3:30C6 Touchpad" "libinput Tapping Enabled" 1 &',
         'goautolock --time 360 --notify 30 --locker "$HOME/.config/awesome/bin/blur.sh" &',
-        'dropbox start &',
-        'pulseeffects --gapplication-service &',
+        'dropbox start &', 'pulseeffects --gapplication-service &',
         '/usr/bin/lxqt-policykit-agent &',
         ' eval "$(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)" &',
-        'xcape -e "Super_L=Super_L|Control_L|Escape" &',
-        'greenclip daemon &',
-        'udiskie &',
-        'picom -b   &',
+        'xcape -e "Super_L=Super_L|Control_L|Escape" &', 'greenclip daemon &',
+        'udiskie &', 'picom -b   &',
         'bash ' .. config_dir .. '/bin/autorun.sh &'
     },
+    -- ########################################################################
     -- List of binaries/shell scripts that will execute for a certain task
     --
     utils = {
+        -- ########################################################################
         -- Fullscreen screenshot
         --
         full_screenshot = config_dir .. '/bin/snapshot.sh full',
+        -- ########################################################################
         -- Area screenshot
         --
         area_screenshot = config_dir .. '/bin/snapshot.sh area',
+        -- ########################################################################
         -- Update profile picture
         --
         update_profile = utils_dir .. '/profile-image'
