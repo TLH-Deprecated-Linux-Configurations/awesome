9cfca5fb1eff3c9cfa0a56151d383d15d512e775
Thomas Highbaugh <thighbaugh@zoho.com>
2022-04-16 02:07:22 -0700

    updates

--- settings/init.lua
+++ settings/init.lua
@@ -6,27 +6,27 @@
 -- ------------------------------------------------- --
 
 return {
-    theme = "vice",
-    background = "7.png",
-    default_programs = {
-        terminal = "kitty",
-        terminal_editor = "code-oss",
-        editor = "nvim",
-        browser = "firefox",
-        file_manager = "thunar",
-        terminal_file_manager = "ranger",
-        lock = config_dir .. "bin/blur.sh",
-        email = "neomutt",
-        chat = "discord",
-        screenshot = config_dir .. "bin/snapshot.sh area"
-    },
-    profile_picture = "user.png",
-    window_gaps = 4,
-    window_border_size = 2,
-    dirs = {
-        downloads = "$HOME/Downloads",
-        pictures = "$HOME/256/Dropbox/Pictures",
-        music = "$HOME/256/Dropbox/Music",
-        wallpapers = "$HOME/.config/awesome/themes/wallpapers"
-    }
+	theme = "vice",
+	background = "9.jpg",
+	default_programs = {
+		terminal = "kitty",
+		terminal_editor = "nvim",
+		editor = "code-oss",
+		browser = "firefox",
+		file_manager = "thunar",
+		terminal_file_manager = "ranger",
+		lock = config_dir .. "bin/blur.sh",
+		email = "neomutt",
+		chat = "discord",
+		screenshot = config_dir .. "bin/snapshot.sh area",
+	},
+	profile_picture = "user.png",
+	window_gaps = 4,
+	window_border_size = 2,
+	dirs = {
+		downloads = "$HOME/Downloads",
+		pictures = "$HOME/256/Dropbox/Pictures",
+		music = "$HOME/256/Dropbox/Music",
+		wallpapers = "$HOME/.config/awesome/themes/wallpapers",
+	},
 }
