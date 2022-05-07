92a21cda37ce2f5d6aa2b309302fef8d4e504a9a
Thomashighbaugh <thighbaugh@zoho.com>
2022-05-07 14:44:16 -0700

    delete tutorial

--- module/init.lua
+++ module/init.lua
@@ -3,15 +3,14 @@
 -- |       ||  _  |  _  ||  |  ||  ||  -__|__ --|
 -- |__|_|__||_____|_____||_____||__||_____|_____|
 -- ------------------------------------------------- --
-local awful = require("awful")
+local awful = require('awful')
 -- ------------------------------------------------- --
-require(... .. ".notifications")
-require(... .. ".menu")
-require(... .. ".tutorial")
-require(... .. ".control-center")
+require(... .. '.notifications')
+require(... .. '.menu')
+require(... .. '.control-center')
 -- ------------------------------------------------- --
-awful.screen.connect_for_each_screen(require(... .. ".notification-center"))
+awful.screen.connect_for_each_screen(require(... .. '.notification-center'))
 --awful.screen.connect_for_each_screen(require(... .. ".bluetooth-center"))
 
-awful.screen.connect_for_each_screen(require(... .. ".exit-screen"))
-awful.screen.connect_for_each_screen(require(... .. ".bar"))
+awful.screen.connect_for_each_screen(require(... .. '.exit-screen'))
+awful.screen.connect_for_each_screen(require(... .. '.bar'))
